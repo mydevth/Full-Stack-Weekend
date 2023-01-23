b5ed46ca07a6e902c8a1ec35a376324cf0839253
mydevth <mydevth@chonburi.biz>
2023-01-23 17:20:34 +0700

    start react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+function App() {
+  return (
+  <header className="header">
+  <div className="logo" >
+    <img src="logo.png" height="68"  widht="68" alt="Today I Learn Logo" />
+    <h1>Today I Learned</h1>
+  </div>
+
+  <button className="btn btn-large btn-open">Share a fact</button>
+  <style></style>
+</header>
+  )
+}
+
+export default App;
